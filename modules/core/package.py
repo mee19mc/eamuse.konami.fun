dcbfc6b2b4de6e644a259d23635f1f79d3df7fb6
mee19mc <85102106+mee19mc@users.noreply.github.com>
2025-10-16 19:40:57 +0800

    prelims update package.*

--- modules/core/package.py
+++ modules/core/package.py
@@ -9,7 +9,7 @@ router = APIRouter(prefix="/core", tags=["package"])
 async def package_list(request: Request):
     request_info = await core_process_request(request)
 
-    response = E.response(E.package(expire=1200, status=0))
+    response = E.response(E.package(expire=1200, status=1(url="" name="" desc="" size="" pkgtype="" sumtype="" sum="" from="" till=""))
 
     response_body, response_headers = await core_prepare_response(request, response)
     return Response(content=response_body, headers=response_headers)
